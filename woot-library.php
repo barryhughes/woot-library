caf3699c51ad555e3ccde922db6e5e81e8196466
barryhughes <barry@codingkills.me>
2014-07-28 16:13:44 -0700

    Loader not called until later in the plugins_loaded request (fixed #2)

--- woot-library.php
+++ woot-library.php
@@ -12,13 +12,13 @@ function woot_library_loader() {
 	global $woocommerce;
 
 	// Preflight checks
-	if (!class_exists('TribeWooTickets')) return;
-	if (!class_exists('TribeEvents') || -1 === version_compare(TribeEvents::VERSION, '3.1')) return;
-	if (!class_exists('WooCommerce') || -1 === version_compare($woocommerce->version, '2.0.13')) return;
+	if ( ! class_exists( 'TribeWooTickets' ) ) return;
+	if ( ! class_exists( 'TribeEvents' ) || -1 === version_compare( TribeEvents::VERSION, '3.1' ) ) return;
+	if ( ! class_exists( 'WooCommerce' ) || -1 === version_compare( $woocommerce->version, '2.0.13' ) ) return;
 
 	// Load
-	require dirname(__FILE__) . '/aliases.php';
-	require dirname(__FILE__) . '/library.php';
+	require dirname( __FILE__ ) . '/aliases.php';
+	require dirname( __FILE__ ) . '/library.php';
 }
 
-add_action('plugins_loaded', 'woot_library_loader');
+add_action('plugins_loaded', 'woot_library_loader', 50);
